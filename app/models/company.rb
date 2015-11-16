27d0d67e7ebf047f6bd02ad7860ee358989135e9
GoaL <gochualex@gmail.com>
2015-11-16 23:36:25 +0000

    Fixed CSV uploader

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,11 +1,12 @@
 class Company < ActiveRecord::Base
-
-
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       Company.create! row.to_hash
     end
   end
-
-
+  #def self.import(file)
+  #  CSV.foreach(file.path, headers: true) do |row|
+  #    Company.create! row.to_hash
+  #  end
+  #end
 end
